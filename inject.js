cdf1cad065498f0b586f2430b55ae0d16c6c59e5
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-09-21 12:36:09 -0400

    This was surprisingly easy

--- inject.js
+++ inject.js
@@ -30,20 +30,31 @@ let mappings = {
 	}
 }
 let active = {};
-
-String.prototype.replaceAt = function(index, replacement) {
-    return this.substr(0, index) + replacement + this.substr(index + replacement.length);
-}
+let toggle = {};
 
 document.addEventListener("keydown", e => {
-	if (!e || !e.key || !e.target || !e.target.selectionStart || !("value" in e.target) || !mappings[e.key] || !mappings[e.key][e.target.value[e.target.selectionStart-1]]) return;
+	if (!e || !e.key || !e.target || !("selectionStart" in e.target) || !("value" in e.target) || !(e.key in mappings)) return;
 	let isActive = active[e.key];
 	active[e.key] = true;
 	if (!isActive) return;
-	e.target.value = e.target.value.replaceAt(e.target.selectionStart-1, mappings[e.key][e.target.value[e.target.selectionStart-1]]);
+	let replacement = mappings[e.key][e.target.value[e.target.selectionStart-1]];
+	if (!replacement) return;
 	e.preventDefault();
+	if (!toggle[e.key]) toggle[e.key] = 0;
+	toggle[e.key] = (toggle[e.key] + 1) % 3;
+	if (toggle[e.key]) return;
+	e.target.selectionStart -= 1;
+	var start = e.target.selectionStart;
+	var finish = e.target.selectionEnd;
+	var allText = e.target.value;
+	var sel = allText.substring(start, finish);
+	var newText = allText.substring(0, start)+replacement+allText.substring(finish, allText.length);
+	e.target.value=newText;
+	e.target.selectionStart = finish;
+	e.target.selectionEnd = finish;
 });
 
 document.addEventListener("keyup", e => {
-	active[e.key] = true;
+	active[e.key] = false;
+	toggle[e.key] = 0;
 });
