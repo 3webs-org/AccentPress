5ba423060749a5c6221a8b99676e018cfe165e5f
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-09-29 11:31:31 -0400

    Config version v2

--- js/inject.js
+++ js/inject.js
@@ -1,7 +1,35 @@
-let remote = "https://pandapip1.github.io/AccentPress/accents.json";
+let remote = "https://pandapip1.github.io/AccentPress/config/accents.json";
 let active = {};
 let toggle = {};
-fetch(remote).then(res => res.json()).then(mappings => {
+fetch(remote).then(res => res.json()).then(raw_mappings => {
+	let mappings = {};
+	Object.keys(raw_mappings).filter(lang => true).forEach(lang => {
+		raw_mappings[lang].forEach(letters => {
+			let baselet = letters[0];
+			let baseletU = letters[0].toUpperCase();
+			if (!mappings[letters[0]]){
+				mappings[baselet] = {};
+				mappings[baseletU] = {};
+				mappings[baselet][baselet] = baselet;
+				mappings[baseletU][baseletU] = baseletU;
+			}
+			letters.forEach(letter => {
+				let letterU = letter.toUpperCase();
+				if (!mappings[baselet]][letter]) Object.keys(mappings[baselet]).forEach(letter2replace => {
+					if (mappings[baselet][letter2replace] == baselet){
+						mappings[baselet][letter2replace] = letter;
+						mappings[baselet][letter] = baselet;
+					}
+				});
+				if (!mappings[baseletU]][letterU]) Object.keys(mappings[baseletU]).forEach(letter2replace => {
+					if (mappings[baseletU][letter2replace] == baseletU){
+						mappings[baseletU][letter2replace] = letterU;
+						mappings[baseletU][letter] = baseletU;
+					}
+				});
+			});
+		});
+	});
 	document.addEventListener("keydown", e => {
 		if (!e || !e.key || !e.target || !("selectionStart" in e.target) || !("value" in e.target) || !(e.key in mappings)) return;
 		let isActive = active[e.key];
@@ -11,7 +39,7 @@ fetch(remote).then(res => res.json()).then(mappings => {
 		if (!replacement) return;
 		e.preventDefault();
 		if (!toggle[e.key]) toggle[e.key] = 0;
-		toggle[e.key] = (toggle[e.key] + 1) % 3;
+		toggle[e.key] = (toggle[e.key] + 1) % 4;
 		if (toggle[e.key]) return;
 		e.target.selectionStart -= 1;
 		var start = e.target.selectionStart;
