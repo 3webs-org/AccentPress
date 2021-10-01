188a1d8923bc7dc8feede5c6bc404aa16e45b8ed
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-10-01 08:37:35 -0400

    Rewrite with vue.js part 2

--- js/popup.js
+++ js/popup.js
@@ -1,53 +1,26 @@
 (async () => {
-  let langs = [
-    'fr'
-  ];
-  let options = {
-    speed: 4
-  };
+  var app = new Vue({
+    el: '#app',
+    data: {
+      langConfig: await fetch("https://accentpress.pandapip1.com/config/languages.json").then(res => res.json()),
+      langs: [ 'fr' ],
+      options: { speed: 4 }
+    },
+    computed: {
+      update: function(){
+        chrome.storage.sync.set({
+          langs: this.langs,
+          options: this.options
+        });
+        return "Saved";
+      }
+    }
+  });
 
   chrome.storage.sync.get(['langs', 'options'], (data) => {
     if (data && data.langs)
-      langs = data.langs;
+      app.langs = data.langs;
     if (data && data.options)
-      options = data.options;
-    langs.forEach(lang => {
-      document.getElementById(lang).checked = true;
-    });
-    Object.keys(options).forEach(option => {
-      document.getElementById(option).value = options[option];
-    });
-    chrome.storage.sync.set({
-      langs: langs,
-      options: options
-    });
-  });
-
-  function addEventListeners(element, type, handler) {
-    if (!element) return;
-    try {
-      element.addEventListener(type, handler);
-    } catch {}
-    Array.from(element.elements).forEach(e => addEventListeners(e, type, handler));
-  };
-
-  addEventListeners(document.getElementById("languagesForm"), "change", event => {
-    if (event.target.checked){
-      langs.push(event.target.id);
-    } else {
-      langs = langs.filter(l => l !== event.target.id);
-    }
-    chrome.storage.sync.set({
-      langs: langs,
-      options: options
-    });
-  });
-
-  addEventListeners(document.getElementById("optionsForm"), "change", event => {
-    options[event.target.id] = event.target.value;
-    chrome.storage.sync.set({
-      langs: langs,
-      options: options
-    });
+      app.options = data.options;
   });
 })();
