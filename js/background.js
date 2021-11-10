ad9ae722d6751a81ce6ab967c48d998a72f95b1e
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-11-10 11:37:30 -0500

    Clean up background.js

--- js/background.js
+++ js/background.js
@@ -1,22 +1,11 @@
-chrome.runtime.onMessageExternal.addListener(
-  function(request, sender, sendResponse) {
-    if (request.type == "GET_DATA") {
-      chrome.storage.sync.get(['langs', 'options'], (data) => {
-        let storage_data = {};
-        if (data && data.langs)
-          storage_data.langs = data.langs;
-        if (data && data.options)
-          storage_data.options = data.options;
-        sendResponse({ success: true, storage_data: storage_data });
-      });
-    }
-    if (request.type == "SET_DATA") {
-      chrome.storage.sync.set(request.storage_data);
-      sendResponse({ success: true });
-    }
+chrome.runtime.onMessageExternal.addListener(async (request, sender, sendResponse) => {
+  if (request.type == "GET_DATA") {
+    sendResponse({ success: true, storage_data: await chrome.storage.sync.get(null) });
+  }
+  if (request.type == "SET_DATA") {
+    await chrome.storage.sync.set(request.storage_data);
+    sendResponse({ success: true });
   }
 );
 
-chrome.action.onClicked.addListener(function(tab) {
-    chrome.tabs.create({'url': "https://accentpress.pandapip1.com/html/popup.html", 'selected': true});
-});
+chrome.action.onClicked.addListener(() => chrome.tabs.create({'url': "https://accentpress.pandapip1.com/html/popup.html", 'selected': true}));
