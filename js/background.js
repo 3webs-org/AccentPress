e2f6d94a5ed651ee73daa5d8c3dc4ae87fb661db
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-11-18 10:13:17 -0500

    ✨ Request user feedback (#4)

--- js/background.js
+++ js/background.js
@@ -1,3 +1,12 @@
+chrome.runtime.onInstalled.addListener(function(details) {
+    if (details.reason == "install") {
+        var uninstallUrlLink = 'https://forms.gle/h2djLMjEC88oK1vq9';
+        if (chrome.runtime.setUninstallURL) {
+            chrome.runtime.setUninstallURL(uninstallUrlLink);
+        }
+    }
+});
+
 chrome.runtime.onMessageExternal.addListener(async (request, sender, sendResponse) => {
   if (request.type == "GET_DATA") {
     sendResponse({ success: true, storage_data: await chrome.storage.sync.get(null) });
