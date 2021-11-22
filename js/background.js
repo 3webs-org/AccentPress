1be649be8b1078d71bb593bbba54ca50fcc8c16e
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-11-22 11:13:55 -0500

    🐛 Error handling for analytics

--- js/background.js
+++ js/background.js
@@ -1,47 +1,49 @@
 async function track(event, props) {
-  // Token ID
-  let token = "a17adb469eb6c2dd0e78217022de6e9d";
-  // Generate random strings
-  function genID(){
-    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    let str = "";
-    for (;str.length < 24;) str += characters.charAt(Math.random() * characters.length);
-    return str;
-  }
-  // Generate user ID
-  let user_id = (await chrome.storage.sync.get('user_id')).user_id;
-  if (!user_id) {
-    user_id = genID();
-    await chrome.storage.sync.set({ user_id });
-  }
-  // Generate properties object
-  let properties = Object.assign(Object.assign({}, props), {
-    "time":        Date.now() / 1000 | 0, // Timestamp in seconds
-    "distinct_id": user_id,               // User ID
-    "$insert_id":  genID(),               // Generate Event ID
-    "token":       token                  // Tracking Token
-  });
-  // Send event
-  return await fetch("https://api.mixpanel.com/track", {
-    "method": "POST",
-    "headers": {
-      "Accept": "text/plain",
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    "body": new URLSearchParams({
-      "data": JSON.stringify({ event, properties }),
-      "verbose": 1
-    })
-  }).then(response => response.json());
+  try {
+    // Token ID
+    let token = "a17adb469eb6c2dd0e78217022de6e9d";
+    // Generate random strings
+    function genID(){
+      let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      let str = "";
+      for (;str.length < 24;) str += characters.charAt(Math.random() * characters.length);
+      return str;
+    }
+    // Generate user ID
+    let user_id = (await chrome.storage.sync.get('user_id')).user_id;
+    if (!user_id) {
+      user_id = genID();
+      await chrome.storage.sync.set({ user_id });
+    }
+    // Generate properties object
+    let properties = Object.assign(Object.assign({}, props), {
+      "time":        Date.now() / 1000 | 0, // Timestamp in seconds
+      "distinct_id": user_id,               // User ID
+      "$insert_id":  genID(),               // Generate Event ID
+      "token":       token                  // Tracking Token
+    });
+    // Send event
+    return await fetch("https://api.mixpanel.com/track", {
+      "method": "POST",
+      "headers": {
+        "Accept": "text/plain",
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      "body": new URLSearchParams({
+        "data": JSON.stringify({ event, properties }),
+        "verbose": 1
+      })
+    }).then(response => response.json());
+  } catch { }
 }
 
 chrome.runtime.onInstalled.addListener(function(details) {
-    if (details.reason == "install") {
-        var uninstallUrlLink = 'https://forms.gle/h2djLMjEC88oK1vq9';
-        if (chrome.runtime.setUninstallURL) {
-            chrome.runtime.setUninstallURL(uninstallUrlLink);
-        }
+  if (details.reason == "install") {
+    var uninstallUrlLink = 'https://forms.gle/h2djLMjEC88oK1vq9';
+    if (chrome.runtime.setUninstallURL) {
+      chrome.runtime.setUninstallURL(uninstallUrlLink);
     }
+  }
 });
 
 chrome.runtime.onMessageExternal.addListener(async (request, sender, sendResponse) => {
