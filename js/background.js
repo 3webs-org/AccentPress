f0ae28a04b7ae7d7e202d9067e4fccb123e09b8b
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-11-30 08:09:21 -0500

    🔈 Error logging

--- js/background.js
+++ js/background.js
@@ -53,78 +53,87 @@ chrome.runtime.onInstalled.addListener(function(details) {
 });
 
 chrome.runtime.onMessageExternal.addListener(async (request, sender, sendResponse) => {
-  if (request.type == "GET_DATA") {
-    // Fetch data
-    let storage_data_local = await chrome.storage.local.get(null);
-    let storage_data_sync = await chrome.storage.sync.get(null);
-    // Make data
-    let defaults = (storage_data_local && storage_data_local.cache && 'defaults' in storage_data_local.cache) ? storage_data_local.cache.defaults : await fetchTimeout("https://accentpress.pandapip1.com/config/defaults.json").then(res => res.json());
-    let speed = (storage_data_sync && storage_data_sync.options && 'speed' in storage_data_sync.options && !Number.isNaN(parseInt(storage_data_sync.options.speed))) ? parseInt(storage_data_sync.options.speed) : defaults.options.speed;
-    let langs = (storage_data_sync && 'langs' in storage_data_sync) ? storage_data_sync.langs : defaults.langs;
-    // Send tracking event
-    track('settings_open', {}).catch(() => {});
-    // Send response
-    sendResponse({
-      success: true,
-      storage_data: {
-        langs: langs,
-        options: {
-          speed: speed
+  try {
+    if (request.type == "GET_DATA") {
+      // Fetch data
+      let storage_data_local = await chrome.storage.local.get(null);
+      let storage_data_sync = await chrome.storage.sync.get(null);
+      // Make data
+      let defaults = (storage_data_local && storage_data_local.cache && 'defaults' in storage_data_local.cache) ? storage_data_local.cache.defaults : await fetchTimeout("https://accentpress.pandapip1.com/config/defaults.json").then(res => res.json());
+      let speed = (storage_data_sync && storage_data_sync.options && 'speed' in storage_data_sync.options && !Number.isNaN(parseInt(storage_data_sync.options.speed))) ? parseInt(storage_data_sync.options.speed) : defaults.options.speed;
+      let langs = (storage_data_sync && 'langs' in storage_data_sync) ? storage_data_sync.langs : defaults.langs;
+      // Send tracking event
+      track('settings_open', {}).catch(() => {});
+      // Send response
+      sendResponse({
+        success: true,
+        storage_data: {
+          langs: langs,
+          options: {
+            speed: speed
+          }
         }
-      }
-    });
-  }
-  if (request.type == "SET_DATA") {
-    // Fetch data
-    let storage_data_local = await chrome.storage.local.get(null);
-    let storage_data_old = await chrome.storage.sync.get(null);
-    let storage_data_new = request.storage_data;
-    // Make data
-    let defaults = (storage_data_local && storage_data_local.cache && 'defaults' in storage_data_local.cache) ? storage_data_local.cache.defaults : await fetchTimeout("https://accentpress.pandapip1.com/config/defaults.json").then(res => res.json());
-    let speed_old = (storage_data_old && storage_data_old.options && storage_data_old.options.speed && !Number.isNaN(parseInt(storage_data_old.options.speed))) ? parseInt(storage_data_old.options.speed) : defaults.options.speed;
-    let langs_old = (storage_data_old && 'langs' in storage_data_old) ? storage_data_old.langs : defaults.langs;
-    let speed_new = (storage_data_new && storage_data_new.options && storage_data_new.options.speed && !Number.isNaN(parseInt(storage_data_new.options.speed))) ? parseInt(storage_data_new.options.speed) : speed_old;
-    let langs_new = (storage_data_new && 'langs' in storage_data_new) ? storage_data_new.langs : langs_old;
-    // Set new data
-    await chrome.storage.sync.set({
-      langs: langs_new,
-      options: {
-        speed: speed_new
-      }
-    });
-    // Send tracking event
-    track('settings_change', {
-      local_storage: storage_data_local,
-      sync_storage: storage_data_new,
-      sync_storage_old: storage_data_old,
-      speed_old: speed_old,
-      speed_new: speed_old,
-      chg_speed: speed_new-speed_old,
-      langs_old: langs_old,
-      langs_new: langs_new,
-      langs_add: langs_new.filter(x => langs_old.indexOf(x) < 0),
-      langs_rem: langs_old.filter(x => langs_new.indexOf(x) < 0)
+      });
+    }
+    if (request.type == "SET_DATA") {
+      // Fetch data
+      let storage_data_local = await chrome.storage.local.get(null);
+      let storage_data_old = await chrome.storage.sync.get(null);
+      let storage_data_new = request.storage_data;
+      // Make data
+      let defaults = (storage_data_local && storage_data_local.cache && 'defaults' in storage_data_local.cache) ? storage_data_local.cache.defaults : await fetchTimeout("https://accentpress.pandapip1.com/config/defaults.json").then(res => res.json());
+      let speed_old = (storage_data_old && storage_data_old.options && storage_data_old.options.speed && !Number.isNaN(parseInt(storage_data_old.options.speed))) ? parseInt(storage_data_old.options.speed) : defaults.options.speed;
+      let langs_old = (storage_data_old && 'langs' in storage_data_old) ? storage_data_old.langs : defaults.langs;
+      let speed_new = (storage_data_new && storage_data_new.options && storage_data_new.options.speed && !Number.isNaN(parseInt(storage_data_new.options.speed))) ? parseInt(storage_data_new.options.speed) : speed_old;
+      let langs_new = (storage_data_new && 'langs' in storage_data_new) ? storage_data_new.langs : langs_old;
+      // Set new data
+      await chrome.storage.sync.set({
+        langs: langs_new,
+        options: {
+          speed: speed_new
+        }
+      });
+      // Send tracking event
+      track('settings_change', {
+        local_storage: storage_data_local,
+        sync_storage: storage_data_new,
+        sync_storage_old: storage_data_old,
+        speed_old: speed_old,
+        speed_new: speed_old,
+        chg_speed: speed_new-speed_old,
+        langs_old: langs_old,
+        langs_new: langs_new,
+        langs_add: langs_new.filter(x => langs_old.indexOf(x) < 0),
+        langs_rem: langs_old.filter(x => langs_new.indexOf(x) < 0)
+      }).catch(() => {});
+      // Send response
+      sendResponse({
+        success: true
+      });
+    }
+    if (request.type == "RESET_DATA") {
+      // Fetch data
+      let storage_data_local = await chrome.storage.local.get(null);
+      let storage_data_sync = await chrome.storage.sync.get(null);
+      // Clear storage
+      await chrome.storage.local.clear();
+      await chrome.storage.sync.clear();
+      // Send tracking event
+      track('settings_clear', {
+        local_storage: storage_data_local,
+        sync_storage: storage_data_sync
+      }).catch(() => {});
+      // Send response
+      sendResponse({
+        success: true
+      });
+    }
+  } catch (e) {
+    // Automatic error reporting
+    track('error_detected', {
+      location: "js.background.js-chrome.runtime.onMessageExternal",
+      error_msg: e.toString(),
+      error_stack: e.stack
     }).catch(() => {});
-    // Send response
-    sendResponse({
-      success: true
-    });
-  }
-  if (request.type == "RESET_DATA") {
-    // Fetch data
-    let storage_data_local = await chrome.storage.local.get(null);
-    let storage_data_sync = await chrome.storage.sync.get(null);
-    // Clear storage
-    await chrome.storage.local.clear();
-    await chrome.storage.sync.clear();
-    // Send tracking event
-    track('settings_clear', {
-      local_storage: storage_data_local,
-      sync_storage: storage_data_sync
-    }).catch(() => {});
-    // Send response
-    sendResponse({
-      success: true
-    });
   }
 });
