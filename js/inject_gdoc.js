7ec84769933f6af08a838cc4ffbf034e29c09308
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2026-09-06 11:41:56 +0000

    Commit Updated Files

--- js/inject_gdoc.js
+++
@@ -1,6 +0,0 @@
-// Modified from: https://github.com/birchill/10ten-ja-reader
-console.info('[Accent Press] Accent Press is requesting Google Docs to use HTML rendering');
-
-const scriptElem = document.createElement('script');
-scriptElem.textContent = `(function() { window['_docs_force_html_by_ext'] = 'nfcdcdoegfnidkeldipgmhbabmndlhbf'; })();`;
-(document.head || document.documentElement).append(scriptElem);
